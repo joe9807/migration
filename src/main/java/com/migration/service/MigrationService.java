1296e4211e1a8c4871342bf10913e0e427a5e97b
Sergey Fedorkov <fedorkov@farzoom.com>
2024-04-13 22:54:58 +0300

    another way of CompletableFuture implementation

--- src/main/java/com/migration/service/MigrationService.java
+++ src/main/java/com/migration/service/MigrationService.java
@@ -3,7 +3,7 @@ package com.migration.service;
 import com.migration.object.GenericObject;
 import org.springframework.stereotype.Component;
 
-import java.util.Collection;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
@@ -13,20 +13,23 @@ import java.util.stream.Stream;
 public class MigrationService {
 
     public CompletableFuture<List<String>> handle(GenericObject object){
-        List<CompletableFuture<List<String>>> futures = Stream.of(CompletableFuture.completedFuture(object).thenApplyAsync(this::getWorkerMethod).thenApplyAsync(List::of)).collect(Collectors.toList());
-        futures.addAll(object.getChildren().stream().map(this::handle).collect(Collectors.toList()));
+        CompletableFuture<List<String>> futureObject = CompletableFuture.completedFuture(object).thenApplyAsync(this::getWorkerMethod);
+        for (GenericObject child:object.getChildren()){
+            futureObject = futureObject.thenCombineAsync(handle(child), (r1, r2)-> Stream.concat(r1.stream(), r2.stream()).collect(Collectors.toList()));
+        }
 
-        return CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]))
-                .thenApplyAsync(name -> futures.stream().map(CompletableFuture::join).flatMap(Collection::stream).collect(Collectors.toList()));
+        return futureObject;
     }
 
-    private String getWorkerMethod(GenericObject object){
+    private List<String> getWorkerMethod(GenericObject object){
         System.out.println(object.getName());
         try {
             Thread.sleep(1);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        return object.getName();
+        List<String> result = new ArrayList<>();
+        result.add(object.getName());
+        return result;
     }
 }
