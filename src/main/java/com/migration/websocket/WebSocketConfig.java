752bfcd59180e16073263103c65996d39835118b
joe9807@gmail.com <joe9807@gmail.com>
2025-05-14 16:19:03 +0300

    remove unused code

--- src/main/java/com/migration/websocket/WebSocketConfig.java
+++ src/main/java/com/migration/websocket/WebSocketConfig.java
@@ -1,7 +1,6 @@
 package com.migration.websocket;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -14,12 +13,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/migration-statistics").setAllowedOriginPatterns("*").withSockJS();
-    }
-
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic");
-        registry.setApplicationDestinationPrefixes("/logs");
     }
 
     @Override
